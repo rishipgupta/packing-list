ccdb1143e665aada003766c5e3f4115e93f5a7b3
rishipgupta <guptariship@gmail.com>
2023-07-30 11:10:23 +0530

    Tested with static content in TravelList Component

---
+++ src/Components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      {item.description}
+    </li>
+  )
+}
+
+export default Item
--- src/Components/TravelList.js
+++ src/Components/TravelList.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Item from './Item';
 
 const TravelList = () => {
+  const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+  ];
+
   return (
+
     <div className='list'>
-      List
+      <ul>
+        {initialItems.map((item) => <Item item={item} />)}
+      </ul>
     </div>
+
   )
 }
 
